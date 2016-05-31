daa0c58bd55a7081d847a681f531841ea0139ef1
DLX <768606115@qq.com>
2016-05-31 21:33:42 +0800

    face recogition project

---
+++ face_recogition/build/CMakeFiles/WebcamFaceRec.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dlx/face/Chapter8_FaceRecognition/ImageUtils_0.7.cpp" "/home/dlx/face/Chapter8_FaceRecognition/build/CMakeFiles/WebcamFaceRec.dir/ImageUtils_0.7.cpp.o"
+  "/home/dlx/face/Chapter8_FaceRecognition/detectObject.cpp" "/home/dlx/face/Chapter8_FaceRecognition/build/CMakeFiles/WebcamFaceRec.dir/detectObject.cpp.o"
+  "/home/dlx/face/Chapter8_FaceRecognition/main.cpp" "/home/dlx/face/Chapter8_FaceRecognition/build/CMakeFiles/WebcamFaceRec.dir/main.cpp.o"
+  "/home/dlx/face/Chapter8_FaceRecognition/preprocessFace.cpp" "/home/dlx/face/Chapter8_FaceRecognition/build/CMakeFiles/WebcamFaceRec.dir/preprocessFace.cpp.o"
+  "/home/dlx/face/Chapter8_FaceRecognition/recognition.cpp" "/home/dlx/face/Chapter8_FaceRecognition/build/CMakeFiles/WebcamFaceRec.dir/recognition.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/dlx/OpenCV/build"
+  "/home/dlx/OpenCV/include"
+  "/home/dlx/OpenCV/include/opencv"
+  "/home/dlx/OpenCV/modules/core/include"
+  "/home/dlx/OpenCV/modules/flann/include"
+  "/home/dlx/OpenCV/modules/imgproc/include"
+  "/home/dlx/OpenCV/modules/highgui/include"
+  "/home/dlx/OpenCV/modules/features2d/include"
+  "/home/dlx/OpenCV/modules/calib3d/include"
+  "/home/dlx/OpenCV/modules/ml/include"
+  "/home/dlx/OpenCV/modules/video/include"
+  "/home/dlx/OpenCV/modules/legacy/include"
+  "/home/dlx/OpenCV/modules/objdetect/include"
+  "/home/dlx/OpenCV/modules/photo/include"
+  "/home/dlx/OpenCV/modules/gpu/include"
+  "/home/dlx/OpenCV/modules/ocl/include"
+  "/home/dlx/OpenCV/modules/nonfree/include"
+  "/home/dlx/OpenCV/modules/contrib/include"
+  "/home/dlx/OpenCV/modules/stitching/include"
+  "/home/dlx/OpenCV/modules/superres/include"
+  "/home/dlx/OpenCV/modules/ts/include"
+  "/home/dlx/OpenCV/modules/videostab/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ face_recogition/build/CMakeFiles/WebcamFaceRec.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/WebcamFaceRec.dir/main.cpp.o"
+  "CMakeFiles/WebcamFaceRec.dir/detectObject.cpp.o"
+  "CMakeFiles/WebcamFaceRec.dir/preprocessFace.cpp.o"
+  "CMakeFiles/WebcamFaceRec.dir/recognition.cpp.o"
+  "CMakeFiles/WebcamFaceRec.dir/ImageUtils_0.7.cpp.o"
+  "WebcamFaceRec.pdb"
+  "WebcamFaceRec"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/WebcamFaceRec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
